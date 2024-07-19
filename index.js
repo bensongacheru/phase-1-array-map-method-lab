e54f508f4febd2239e7a283c1eda4b52d554a13f
benson <mwangibenson923@gmail.com>
2024-07-19 14:47:01 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
+
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What Is The Constructor OO Pattern?",
+   "Implementing Blockchain Web API" ,
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "What Is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(title => {
+    let words = title.split(' ');
+    let capitalizedWords = words.map(word => {
+      if (word === 'NAN') {
+        return 'NaN'
+      } else {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }
+    });
+    return capitalizedWords.join(' ');
+  });
 }
+
